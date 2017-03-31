a8349bb57cea3fa41e29284b35330489af7306ce
derekortiz <ortizder@gmail.com>
2017-03-31 12:28:54 -0400

    Omit Boostrap #collapse from scrollto to avoid it hijacking the collapse function in FAQ

--- _assets_/js/scripts.js
+++ _assets_/js/scripts.js
@@ -266,7 +266,7 @@
 		onScrollInit($('.os-animation'));
 
 		//#Smooth Scrolling
-		$('a[href*=#]:not([href=#])').click(function() {
+		$('a[href*=#]:not([href=#],[href*="#collapse"])').click(function() {
 			if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
 				var target = $(this.hash);
 				target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
