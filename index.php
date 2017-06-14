2863754bb636fadfc8e49076e013c61425e83897
derekortiz <ortizder@gmail.com>
2017-06-14 16:38:23 -0400

    Add missing div for share widget
    Clean up some some formatting for the share widget

--- index.php
+++ index.php
@@ -188,40 +188,41 @@
         <h4 class="modal-title" id="myModalLabel">Share this page</h4>
       </div>
       <div class="modal-body">
-	      <div class="copylink">
-		      <p>Copy and past this code into your website.</p>
-		      <pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
-	      </div><!-- /.copylink -->
-				<div class="share-btns">
-		      <p>Share this page on your favorite Social network</p>
-		      <div class="row">
-			      <div class="col-md-3 col-xs-6">
-							<a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
-    target="_blank">
-								<i class="fa fa-facebook"></i> Facebook
-							</a>
-						</div>
-			      <div class="col-md-3 col-xs-6">
-							<a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
-    target="_blank">
-								<i class="fa fa-twitter"></i> Twitter
-							</a>
-						</div>							
-			      <div class="col-md-3 col-xs-6">
-						<a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
-    target="_blank">
-							<i class="fa fa-google-plus"></i> Google Plus
-						</a>
-						</div>						
-			      <div class="col-md-3 col-xs-6">
-						<a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
-    target="_blank">
-							<i class="fa fa-reddit"></i> Reddit
-						</a>
-						</div>
-					</div><!-- /.row -->
-	      </div><!-- /.share-btns -->
+      <div class="copylink">
+	    <p>Copy and past this code into your website.</p>
+	    <pre>&lt;a href="<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>">Your Link Name&lt;/a&gt;</pre>
+      </div><!-- /.copylink -->
+		<div class="share-btns">
+	      <p>Share this page on your favorite Social network</p>
+	      <div class="row">
+	      	<div class="col-md-3 col-xs-6">
+			  <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-facebook" onclick="return !window.open(this.href, 'facebook ', 'width=500,height=500')"
+target="_blank">
+			  <i class="fa fa-facebook"></i> Facebook
+				</a>
+			</div>
+			<div class="col-md-3 col-xs-6">
+			  <a href="https://www.twitter.com/home?status=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-twitter" onclick="return !window.open(this.href, 'twitter ', 'width=500,height=500')"
+target="_blank">
+				<i class="fa fa-twitter"></i> Twitter
+			  </a>
+			</div>							
+		    <div class="col-md-3 col-xs-6">
+			  <a href="https://www.plus.google.com/share?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-google" onclick="return !window.open(this.href, 'google ', 'width=500,height=500')"
+	target="_blank">
+				<i class="fa fa-google-plus"></i> Google Plus
+			  </a>
+			</div>						
+	        <div class="col-md-3 col-xs-6">
+			  <a href="https://www.reddit.com/submit?url=<?php echo 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']; ?>" class="btn-reddit" onclick="return !window.open(this.href, 'redit ', 'width=500,height=500')"
+target="_blank">
+				<i class="fa fa-reddit"></i> Reddit
+			  </a>
+			</div>
+		  </div><!-- /.row -->
+	    </div><!-- /.share-btns -->
       <button type="button" class="btn btn-success btn-lg" data-dismiss="modal">Close</button>
+      </div><!-- /.modal-body -->
     </div>
   </div><!-- /.modal-dialog -->
 </div><!-- /.modal -->
