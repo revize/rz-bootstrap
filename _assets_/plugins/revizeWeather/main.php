3a6880af7625cb7babfe4b15e24a652062b2ebb7
shortnd <ococncol@gmail.com>
2018-05-25 15:24:17 -0400

    updated weather config

--- _assets_/plugins/revizeWeather/main.php
+++ _assets_/plugins/revizeWeather/main.php
@@ -1,7 +1,7 @@
 <?php
 // error_reporting(E_ALL);ini_set('display_errors', 1);
-require_once __DIR__."\class\OpenWeatherMapWidget.php";
-require_once __DIR__."\config.php";
+require_once __DIR__."/class/OpenWeatherMapWidget.php";
+require_once __DIR__."/config.php";
 header('Content-Type: application/json');
 
 /******************
