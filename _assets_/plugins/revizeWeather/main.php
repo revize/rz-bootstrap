6f079ef26f93bc9986f60b4fa778839bb59e4da1
Derek Ortiz <ortizder@gmail.com>
2018-06-21 13:28:10 -0400

    Change directory seprators from "\" to "/"
    Change will prevent pathing compatibility issues on Unix systems

--- _assets_/plugins/revizeWeather/main.php
+++ _assets_/plugins/revizeWeather/main.php
@@ -1,8 +1,8 @@
 <?php
 // error_reporting(E_ALL);ini_set('display_errors', 1);
-require_once __DIR__."\class\OpenWeatherMapWidget.php";
-require_once __DIR__."\config.php";
-header('Content-Type: application/json');
+require_once __DIR__."/class/OpenWeatherMapWidget.php";
+require_once __DIR__."/config.php";
+header('Content-Type: application/json; charset=UTF-8');
 
 /******************
 
@@ -14,16 +14,15 @@ START MAIN FUNCTION
 try {
 	if( !defined("API_KEY") || API_KEY === "key_goes_here" || API_KEY === "" ) {
 		throw new Exception("API_KEY needs to be defined in config.", 1);
-	} 
-	
+	}
+
 	$options = $_GET;
 	$options['apiKey'] = API_KEY;
 	$weatherWidget = new OpenWeatherMapWidget($options);
 	$response = $weatherWidget->getData();
 
-	// print "<pre>";
 	print $response;
-	// print "</pre>";
+
 } catch (Exception $e) {
 	$response = array( "error" => "$e");
 	print json_encode($response);
