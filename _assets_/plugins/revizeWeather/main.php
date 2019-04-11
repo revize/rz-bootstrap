e87f34835b56951474b8797dd82f5e7701797581
Collin OConnell <ococncol@gmail.com>
2019-04-11 15:52:20 -0400

    fixed main weather and added keyboard navigation

--- _assets_/plugins/revizeWeather/main.php
+++ _assets_/plugins/revizeWeather/main.php
@@ -2,6 +2,7 @@
 // error_reporting(E_ALL);ini_set('display_errors', 1);
 require_once __DIR__."/class/OpenWeatherMapWidget.php";
 require_once __DIR__."/config.php";
+header('Content-Type: application/json; charset=UTF-8');
 
 /******************
 
