2345f8690df5d71e8407dcdf274048477b5a1245
madolinn <madolinnsoup@gmail.com>
2021-03-01 16:20:07 -0500

    Update DT_bootstrap2.js (#18)
    Set default staff directory length to 50 entries

--- _assets_/js/DT_bootstrap2.js
+++ _assets_/js/DT_bootstrap2.js
@@ -103,7 +103,8 @@
 				$('#example').dataTable( {
 					"sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row-fluid'<'span6'i><'span6'p>>",
 					"sPaginationType": "bootstrap",
-					"aaSorting": [[0,'asc']]
+					"aaSorting": [[0,'asc']],
+					"iDisplayLength":50
 				} );
 			} );
 
