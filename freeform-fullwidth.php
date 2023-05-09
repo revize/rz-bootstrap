4f5a9f2cfaf214ffa62d4314161d7356f9dbe38c
madolinn <madolinnsoup@gmail.com>
2023-05-09 15:39:50 -0400

    Move JQuery into Header

--- freeform-fullwidth.php
+++ freeform-fullwidth.php
@@ -15,6 +15,7 @@
 	<link rel="stylesheet" href="_assets_/fonts/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="_assets_/css/animate.min.css">
 	<link rel="stylesheet" href="_assets_/css/layout.css">
+	<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 
 	<link rel="shortcut icon" href="_assets_/images/favicon.ico">
 	<link rel="apple-touch-icon" href="_assets_/images/touch-icon-iphone.png">
@@ -27,7 +28,6 @@
 
 	<!-- Content Here from freeform -->
 
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="_assets_/plugins/modernizr/modernizr.custom.js"></script>
 <script src="_assets_/plugins/tiny-slider/dist/min/tiny-slider.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
